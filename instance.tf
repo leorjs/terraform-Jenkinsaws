5e3a14e21033f1438502ee32048369f33687648d
Leonardo Rojas <119978221+leorjs@users.noreply.github.com>
2024-04-14 17:45:23 -0300

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "jenkins_instance" {
   key_name      = aws_key_pair.jenkins_key_pair.key_name
 
   subnet_id              = aws_subnet.jenkins_subnet.id       # Referencia a la subred creada en la VPC
-  vpc_security_group_ids = [aws_security_group.jenkins_sg.id] # Corregido para usar el ID del grupo de seguridad VPC
+  vpc_security_group_ids = [aws_security_group.jenkins_sg.id]
 
   user_data = file("${path.module}/install_jenkins.sh")
 
